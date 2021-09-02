c47b031edb20ca1d0187b90c247b74ee00fc077b
Alina Dubovskaya <daxelka@iMac.station>
2021-09-02 09:53:03 +0100

    small fix to is_not_converget function

--- Deffuant/deffuant.py
+++ Deffuant/deffuant.py
@@ -15,7 +15,7 @@ class DeffuantModel:
         # Deffuant parameters
         self.confidence = confidence_interval  # restricted to interval (0, 0.5]
         self.cautiousness = cautiousness  # convergence parameter, restricted to interval (0, 0.5]
-        self.PRECISION = 0.01   # difference between opinions that are considered identical
+        self.PRECISION = 0.01  # difference between opinions that are considered identical
         # clusters parameters
         self.CLUSTER_PRECISION = 0.05  # difference in neighbouring opinions belonging to the same cluster
         self.CLUSTER_MAX_LENGTH = 0.2
@@ -63,9 +63,8 @@ class DeffuantModel:
                     n_idle_steps = 0
 
             total_steps += 1
-            not_convergence = self.is_not_convergence(n_idle_steps, total_steps)
+            not_convergence, n_idle_steps = self.is_not_convergence(n_idle_steps, total_steps)
         return self.G
-
 
     def is_not_convergence(self, n_idle_steps, total_steps):
         if total_steps < self.MAXIMUM_STEPS:
@@ -74,14 +73,14 @@ class DeffuantModel:
                 if np.all(np.diff(means) > self.confidence):
                     print(np.all(np.diff(means) > self.confidence))
                     print('model has converged, steps performed:', str(total_steps))
-                    return False
+                    return False, n_idle_steps  # model converged, opinion formation stops
                 else:
-                    return True
+                    return True, 0  # not converged, and restart idle steps counter
             else:
-                return True
+                return True, n_idle_steps  # not converged, continue counting idle steps
         else:
             print('model not converging, maximum steps performed:', str(total_steps))
-            return False
+            return False, n_idle_steps  # not converged, opinion formation stops
 
     def clusters_detector(self, opinion):
         clusters = []
@@ -101,12 +100,9 @@ class DeffuantModel:
             curr_point = point
         clusters.append(curr_cluster)
         means.append(np.mean(curr_cluster))
-        return clusters,  means
-
+        return clusters, means
 
     def show_opinion_distribution(self, opinions):
-        # opinions = np.array(list(nx.get_node_attributes(self.G, 'opinion').values()))
-        # hist_data = np.histogram(opinions.values())
         bins = [0.01 * n for n in range(100)]
         plt.hist(opinions, bins=bins)
         plt.title("Histogram of opinions")
@@ -114,8 +110,7 @@ class DeffuantModel:
         # plt.savefig('result2.png')
 
     def set_opinion(self, opinion_array):
-        # transforming numpy.array into dictionary
-        d = dict(enumerate(opinion_array))
+        d = dict(enumerate(opinion_array))  # transforming numpy.array into dictionary
         nx.set_node_attributes(G, d, 'opinion')
 
     def get_opinion(self):
@@ -124,12 +119,12 @@ class DeffuantModel:
 
 
 # Initiating a graph
-N_nodes: int = 5000
+N_nodes: int = 1000
 G = nx.complete_graph(N_nodes)
 # G = nx.empty_graph(N_nodes)
 
 # Initiating a model on the graph
-model = DeffuantModel(G, 0.2, 0.5)
+model = DeffuantModel(G, 0.4, 0.5)
 
 
 def random_opinion(N_nodes):
@@ -143,19 +138,22 @@ def uniform_opinion(N_nodes):
     opinion = rng.uniform(0.0, 1.0, (N_nodes,))
     return opinion
 
+
 def sin_opinion(N_nodes):
     # randomly chosen N_nodes numbers from [0,1) from uniform distribution
     rng = np.random.default_rng()
     values = rng.uniform(0.0, 1.0, (N_nodes,))
     m = 1
     # opinion_distribution = values + m * np.sin(2*math.pi/k * np.linspace(0, 1, N_nodes, endpoint=False))
-    opinion = m * np.arccos(np.linspace(-1, 1, N_nodes))/math.pi
+    opinion = m * np.arccos(np.linspace(-1, 1, N_nodes)) / math.pi
     return opinion
+
 
 def normal_opinion(N_nodes, mu, sigma):
     rng = np.random.default_rng()
     opinion = rng.normal(mu, sigma, N_nodes)
     return opinion
+
 
 def multimodal_normal_opinion(N_nodes, sigma):
     rng = np.random.default_rng()
@@ -164,6 +162,7 @@ def multimodal_normal_opinion(N_nodes, sigma):
     for mu in mus:
         opinion += rng.normal(mu, sigma, N_nodes)
     return opinion
+
 
 t0 = time.time()
 # Setting initial opinion
@@ -175,7 +174,7 @@ model.set_opinion(initial_opinion)
 # Run the model
 model.opinion_formation()
 t1 = time.time()
-print('performance time:', t1-t0)
+print('performance time:', t1 - t0)
 # Clusters in final opinion
 clusters, means = model.clusters_detector(model.get_opinion())
 print('Means of clusters:', means)
@@ -193,6 +192,3 @@ model.show_opinion_distribution(model.get_opinion())
 #         model.opinion_formation()
 #         # Get final opinion
 #         model.show_opinion_distribution(model.get_opinion())
-
-
-
